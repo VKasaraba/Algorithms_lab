75b359d96ebe05bd964074998e566c50a7062efd
VKasaraba <kasaraba.volodymyr@gmail.com>
2020-12-24 22:45:49 +0200

    added lab 5

---
+++ lab5/runner.py
@@ -0,0 +1,6 @@
+from naive_algorithm import find_substring_positions
+
+if __name__ == '__main__':
+    my_string = 'Internet Of Things'
+    my_pattern = 'i'
+    print(find_substring_positions(my_string, my_pattern))
